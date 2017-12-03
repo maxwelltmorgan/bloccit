2a5193c44ededea534020e4dbe795fbf8c8b9d0c
Maxwell Morgan <maxwelltmorgan@gmail.com>
2017-12-03 03:05:01 -0700

    assignment 24

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  post 'users/confirm' => 'users#confirm'
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
